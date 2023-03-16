6ea368dac45a0a361a2516aefcf366ef011cc126
musaab moha <111170428+musaabmoha@users.noreply.github.com>
2023-03-16 09:21:13 +0300

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const array = process.argv.slice(2);
+const array = process.argv.slice(2); //The process.argv property is an array that contains the command-line arguments passed to the Node.js process.
+//We slice the array starting from the third item to get only the arguments passed to our script.
 if (array[0] === undefined && array[1] === undefined) {
   console.log('undefined is undefined');
 } else if (array[0] !== undefined && array[1] !== undefined) {
